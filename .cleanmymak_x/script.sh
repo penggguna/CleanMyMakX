f2f08f753d10fa7866fd95ec58d4a8dab15a400c
Muhammad Dary Azhari <mdazhari@bk.ru>
2018-09-27 13:19:07 +0700

    Inisialisasi pertama

---
+++ .cleanmymak_x/script.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#    _______                __  ___       __  ___     __     _  __
+#   / ___/ /__ ___ ____    /  |/  /_ __  /  |/  /__ _/ /__  | |/_/
+#  / /__/ / -_) _ `/ _ \  / /|_/ / // / / /|_/ / _ `/  '_/ _>  <  
+#  \___/_/\__/\_,_/_//_/ /_/  /_/\_, / /_/  /_/\_,_/_/\_\ /_/|_|  
+#                               /___/                             
+
+tampilkanText(){
+  echo "   _______                __  ___       __  ___     __     _  __ "
+  echo "  / ___/ /__ ___ ____    /  |/  /_ __  /  |/  /__ _/ /__  | |/_/ "
+  echo ' / /__/ / -_) _ `/ _ \  / /|_/ / // / / /|_/ / _ `/  _/  _>  <   '
+  echo " \___/_/\__/\_,_/_//_/ /_/  /_/\_, / /_/  /_/\_,_/_/\_\ /_/|_|   "
+  echo "  mdazhari@bk.ru              /___/                              "
+}
+
+clear
+tampilkanText
+
+echo ''
+echo 'Program ini akan melakukan pembersihan file yang tidak berguna'
+echo 'Ingin melihat detail kapasitas drive anda? (y/t)'
+read -r lihatdetail
+if [ "$lihatdetail" = y ]
+then
+  echo 'Membuka...'
+  open /Applications/Utilities/Disk\ Utility.app/
+fi
+echo 'Lanjutkan untuk melakukan pembersihan? (y/t)'
+read -r persetujuan
+
+case $persetujuan in
+  'y')  
+    echo 'Menuju direktori user...'
+    echo 'Dan menghapus cache user'
+    cd ~/
+    find .cache
+    ( set -x; pwd; rm -rf .cache)
+
+    echo "Menuju ke direktori library user..."
+    cd ~/Library/
+    find Caches/
+    echo 'Dan menghapus cache user'
+    ( set -x; pwd; rm -rf Caches/)
+
+    echo "Menuju ke direktori library user..."
+    cd ~/Library/
+    find Cookies/
+    echo 'Dan menghapus cookie users'
+    ( set -x; pwd; rm -rf Cookies/)
+
+    echo "Menuju ke direktori library sistem..."
+    cd /Library/
+    find Caches/
+    echo 'Dan menghapus cache sistem'
+    ( set -x; pwd; rm -rf Caches/)
+
+    echo "Menuju ke direktori library sistem..."
+    cd /Library/
+    find Cookies/
+    echo 'Dan menghapus cookie sistem'
+    ( set -x; pwd; rm -rf Cookies/)
+
+    echo "Menuju ke direktori xcode..."
+    cd ~/Library/Developer/Xcode
+    find DerivedData/
+    echo 'Menghapus derived data xcode'
+    ( set -x; pwd; rm -rf DerivedData/)
+    find DocumentationCache/
+    echo 'Dan menghapus documentation cache xcode'
+    ( set -x; pwd; rm -rf DocumentationCache/)
+
+    echo 'Menuju ke tempat sampah user'
+    cd ~/.Trash
+    find .
+    echo 'Menghapus tempat sampah user'
+    ( set -x; pwd; rm -rf *)
+
+    echo '\n\n'
+    tampilkanText
+    echo '\n\n PEMBERSIHAN SELESAI'
+    open /Applications/Utilities/Disk\ Utility.app/
+
+  ;;
+  't')  echo 'Anda tidak mau'
+  ;;
+  *) /script.sh
+  ;;
+esac
---
+++ README.md
@@ -0,0 +1,15 @@
+# CLEAN MY MAK X
+
+## Intro
+Clean My Mak X Adalah Aplikasi Pembersihan File Sistem/User Yang Tidak Berguna
+
+## Pemasangan
+Buka terminal dan jalankan file run dengan ketik
+./run
+Jika sudah terpasang, Clean My Mak X bisa dijalankan dengan membuka tab baru dengan perintah
+cleanmymak
+
+
+## Troubleshoot
+Jika permission denied, solusi ketik
+chmod +x run
